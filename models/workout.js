16f8010505dd2632a7f0cc7ee980cdcf7ee8ae36
Zak Monnet <zakmonnet@gmail.com>
2020-12-02 12:35:32 -0600

    created initial file structure and server functionality

---
+++ models/workout.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: "Enter a name for workout"
+    },
+})
---
+++ routes/api.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+const Transaction = require("../models/workout.js");
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+// router.post("/api/workouts/bulk", ({ body }, res) => {
+//   Transaction.insertMany(body)
+//     .then(dbTransaction => {
+//       res.json(dbTransaction);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
