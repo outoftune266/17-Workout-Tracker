ae1729c25fb91853a39cbdaa2ce2815a5b87a8f6
Zak Monnet <zakmonnet@gmail.com>
2020-12-02 13:38:30 -0600

    built workout model

--- models/workout.js
+++ models/workout.js
@@ -3,9 +3,37 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    name: {
+    exerciseType: {
+        type: String,
+    },
+
+    excerciseName: {
         type: String,
         trim: true,
         required: "Enter a name for workout"
     },
-})
+
+    distance: {
+        type: Number
+    },
+
+    duration: {
+        type: Number
+    },
+
+    weight: {
+        type: Number
+    },
+
+    sets: {
+        type: Number
+    },
+
+    reps: {
+        type: Number
+    }
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
