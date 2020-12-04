3094de4919a60b4227552ac72356ccd8af9253ec
Zak Monnet <zakmonnet@gmail.com>
2020-12-04 14:17:15 -0600

    added update routes

--- routes/api.js
+++ routes/api.js
@@ -11,15 +11,15 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
-// router.post("/api/workouts/bulk", ({ body }, res) => {
-//   Transaction.insertMany(body)
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
+router.put("/api/workouts/:id", ({ body }, res) => {
+  Workout.updateOne(body)
+  .then(dbTransaction => {
+    res.json(dbTransaction);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
 
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
